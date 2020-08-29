1029363afbcbd913dd0e0dffa8770ef11869b3dd
Agustin <ag.fierro@hotmail.com>
2020-08-29 19:53:02 -0300

    arreglos

--- app/src/index.js
+++ app/src/index.js
@@ -21,41 +21,29 @@ app.use(express.static( __dirname + "/static"));
 
 
 
-app.post('/crearPersonas',(req, res) =>{
+app.post('/crearPersonas', (req, res) =>{
 
-    console.log(req.body);
+    console.log(req.body);  
 
     var temp = req.body;    
+ 
+        //Validaciones sobre la informacion ingresada
+        if( Object.keys(temp).length > 3 ){
+            // si la cantidad de atributos dentro del objeto temporal es mayor a los necesarios.
+            res.status(400);
+            res.send("Solo se admiten 3 campos (Nombre, Apellido y Dni) ");
+            return;
+        }
 
-    if( Object.keys(temp).length > 3 ){
-        // si la cantidad de atributos dentro del objeto temporal es mayor a los necesarios.
-        res.status(400);
-        res.send("Solo se admiten 3 campos (Nombre, Apellido y Dni) ");
-    }
-
-    if(!temp.nombre || !temp.apellido){
-        // si no existe un nombre o un apellido.
-        res.status(400);
-        res.send("Se debe ingresar un nombre y un apellido");
-        return;
-    }
-
-    if(!vd.validarTipo(temp.nombre,'string') || !vd.validarTipo(temp.apellido,'string')){
-        // si el nombre o el apellido no son tipo string.
-        res.status(400);
-        res.send("El nombre y el apellido deben ser una cadena de texto");
-        return;
-    }
-
-    if( !vd.validarDni(temp.dni, 10 )){
-        // si el dni no es un numero o si tiene mas de 10 dígitos.
-        res.status(400);
-        res.send("El dni debe ser un numero con menos de 10 caracteres");
-        return;
-    }
+        if( !vd.validarDni(temp.dni, 10 )){
+            // si el dni no es un numero o si tiene mas de 10 dígitos.
+            res.status(400);
+            res.send("El dni debe ser un numero con menos de 10 caracteres");
+            return;
+        }
 
     // Todo okay envio codigo 200 y el objeto que se ingresó.
-    res.status(200)
-    res.send(temp); 
+    res.status(200);
+    res.send(temp);     
 
 })
